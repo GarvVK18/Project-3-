a2d41697ea5ffa927ca4e5693fc4f8ad4ec678fb
Samyuktha <saisamyuktha608@gmail.com>
2026-08-28 19:42:07 +0530

    Added authentication controller

---
+++ src/main/java/com/iam/server/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.iam.server.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iam.server.entity.User;
+import com.iam.server.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+
+        User savedUser = userService.saveUser(user);
+
+        return ResponseEntity.ok(savedUser);
+    }
+}
